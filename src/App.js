a7f7fcd6048a99172a7be1e514abf61c390d913e
Gunes <51853908+Gunes123@users.noreply.github.com>
2022-07-09 20:50:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-
 import './App.css';
 import DataGrid from './components/DataGrid/DataGrid';
 import Provider from './context/Provider/Provider';
 function App() {
   return (
     <div className="App">
-      {<Provider>
-       <DataGrid/>
-       </Provider>
-      }
+      {<Provider><DataGrid /></Provider>}
     </div>
   );
 }
