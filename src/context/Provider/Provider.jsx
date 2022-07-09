9a8216f2e5c1aa01b1c7c40b860e467759e27826
Gunes <51853908+Gunes123@users.noreply.github.com>
2022-07-09 20:54:18 +0300

    Update Provider.jsx

--- src/context/Provider/Provider.jsx
+++ src/context/Provider/Provider.jsx
@@ -1,21 +1,18 @@
+import React, {useReducer, createContext} from 'react'
+import {reducer} from '../../context/Reducer/Reducer'
+export const store = {
+  path: ""
+}
+export const DataContext = createContext(store)
 
-import React,{useReducer,createContext} from 'react'
-import { reducer } from '../../context/Reducer/Reducer'
-export const store= {path:""}
-export const DataContext=createContext(store)
+const Provider = props => {
+  const [state, dispatch] = useReducer(reducer, DataContext)
 
-
-
-
-const Provider = props =>{
-    const [state,dispatch]=useReducer(reducer,DataContext)
-    return(
-        <DataContext.Provider value={{state,dispatch}}>
-           
-                {props.children}
-          
-        </DataContext.Provider>
-    )
+  return (
+    <DataContext.Provider value={{state, dispatch}}>
+      {props.children}
+    </DataContext.Provider>
+  )
 }
 
 export default Provider;
